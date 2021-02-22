27ea597093ab5149e6e463ad8b49b9d0e41f8460
Vanshika Assudani <vanshika.assudani@coditas.com>
2021-02-22 19:41:31 +0530

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { person as Person} from './Person/Person'; //named import
 //import Person from './Person/Person'; //default import
+import Forms from './forms/Forms';
 
 class App extends Component {
    constructor(props){
    super()
     this.state={
       persons:[
-        {name:'Vanshika' , age:22},
+        {name:'vans' , age:22},
         {name:'Vaibhav' , age:21},
         {name:'Vansh' , age:19}
       ],
@@ -54,11 +55,12 @@ handleNewVideos(){
 
   render() 
   {
-    console.log(this)
+    
     return (
       <div className="App">
+      <Forms/>
       {/* This is an inline css block */}
-      <h1 style={{fontWeight:'bold'}}>Hi I am React</h1> 
+      {/*  <h1 style={{fontWeight:'bold'}}>Hi I am React</h1> 
       <div onClick={this.handleVideos}>Handle Videos</div>
       <div onClick={()=>this.handleNewVideos()}>Handle</div>
       <h1 className="title">{this.state.otherState}</h1>
@@ -66,7 +68,8 @@ handleNewVideos(){
       <button onClick={this.switchNameHandler}>Switch Names</button>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies:Dancing </Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>*/}
+     
       </div>
     );
   }
---
+++ src/forms/Forms.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+// a default class 
+export default class Forms extends Component{
+    constructor(props){
+        super(props)
+        //Created a constructor with props parameter , after that gave a super class to use those props in the class forms
+        this.state={
+            // form:{
+            //     title:'',
+            //     body:''
+            // }
+            title:'',
+            body:''
+        }
+    }
+//    handleChange=(e)=>{
+//         e.preventDefault()
+//         const name=e.target.name
+//         const value=e.target.value
+//         this.setState({form:{
+//             ...this.state.from,
+//             [name]:value
+//           }  })
+//     }
+
+//     onClickHandler=(e)=>{
+//         e.preventDefault()
+//         console.log(this.state.form)
+        
+//     }
+//Your state is an object value thats why we are able to access it with the bracket notation.
+/* 
+let num{
+    num1:1,
+    num2:2
+}
+num.num2=5 //dot notation for assigning values.
+num['num2']=10 //bracket notation for assigning values
+
+*/
+//we are getting an input event on the onchange event 
+  handleChange=(e)=>{
+      console.log(e,'Input Change for title')
+      //Assigning a name variable for the target input attribute name
+      const name=e.target.name
+      //Assigning the value for every input attribute
+      const value=e.target.value
+      this.setState({
+          //we are using destructuring and assigning values 
+        [name]:value
+      },
+      
+    //   ()=>{console.log('print state' ,this.state.title,this.state.body)}
+   )
+   console.log('print values' ,this.state.title,this.state.body)
+    }
+
+    render(){
+        return(
+            <div>
+            <h2>Form</h2>
+            <form >
+            <label>
+            Title:
+            { /*<input type="text" value={this.state.form.title} onChange={this.handleChange} />*/}
+            <input type="text" name='title' value={this.state.title} onChange={this.handleChange} />
+          </label>
+          <label>
+          Body:
+           { /* <input type="text" value={this.state.form.body} onChange={this.handleChange} /> />*/}
+           <input type="text" name='body' value={this.state.body} onChange={this.handleChange} />
+        </label>
+            <button onClick={this.onClickHandler}></button>
+            </form>
+            </div>
+        )
+    }
+}
